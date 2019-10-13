d246e4f984edf4fd97982f2ac9920fb05016ea76
yhong <ongyi@oregonstate.edu>
2019-10-12 23:03:42 -0700

    need to test linear reg and ridge reg

--- linear_regression.py
+++ linear_regression.py
@@ -22,15 +22,19 @@ def import_data(file):
 	# print(cols)
 	cols.remove("Chance of Admit ") # remove chance of admit because we are going to predict chance of admit	
 	cols.remove("Serial No.") # remove serial no. because there is no correlation
-	x = np.array(data["GRE Score"]) # passed in column names set that does not have price
+	# x = np.array(data["GRE Score"]) # passed in only GRE score set as inputs
+	x = np.array(data[cols]) # passed in all columns 
 	y = np.array(data["Chance of Admit "]) # get label data "price"
 	return x, y
 
-def linear_regression(x, y):
+'''
+Simple linear regression that import LinearRegression Model from sklearn library
+'''
+def linear_regression_sklearn(x, y):
 
 	# split data into train and test sets, make test data set to be 1/3 of the entire dataset
-	x_train, x_test, y_train, y_test = train_test_split(x.reshape(-1,1), y, test_size=1/3, random_state=0)
-	
+	x_train, x_test, y_train, y_test = train_test_split(x.reshape(-1, 1), y, test_size=1/3, random_state=0)
+
 	# fit simple linear regression to training set
 	regressor = LinearRegression()
 	regressor.fit(x_train, y_train)
@@ -38,6 +42,14 @@ def linear_regression(x, y):
 	# prediction
 	pred =regressor.predict(x_test)
 	
+	print(pred)
+	print(y_test)
+	count = 0
+	for i in range(len(y_test)):
+		if abs(pred[i] - y_test[i]) < 0.01:
+			count += 1
+
+	print(count / len(y_test))
 	# pdb.set_trace()
 	# Visualization of the training dataset
 	plt.scatter(x_train, y_train, color="red")
@@ -56,6 +68,48 @@ def linear_regression(x, y):
 	plt.show()
 
 
+
+class Linear_Model():
+	def __init__(self, x, y, epsilon, Lambda):
+		# x is (M, N) matrix, y is (1, N) matrix
+		self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=1/3, random_state=0)
+		self.epsilon = epsilon
+		self.Lambda = Lambda
+
+	'''
+	Linear regression without regularization term
+	'''	
+	def linear_regression(self):
+		# initialize w with random numbers using numpy
+		w = np.random.rand(len(self.x_train.shape[1]))
+		
+		while True:
+			# Get derivative of sum or squared error 
+			self.sse_grad = (w * self.x_train - self.y_train)*self.x_train
+			# update weights (parameters)
+			w -= self.Lambda * self.sse_grad
+			# check if gradient of sse converges
+			if np.linalg.norm(self.sse_grad) <= self.epsilon:
+				break
+		
+	'''
+	Ridge regression (L2 regularization)
+	'''
+	def ridge_regression(self):
+		# initialize w with random numbers using numpy
+		w = np.random.rand(len(self.x_train.shape[1]))
+		
+		while True:
+			# Get derivative of sum or squared error 
+			self.sse_grad = (w * self.x_train - self.y_train)*self.x_train + 2 * self.Lambda * np.linalg.norm(w) 
+			# update weights (parameters)
+			w -= self.Lambda * self.sse_grad
+			# check if gradient of sse converges
+			if np.linalg.norm(self.sse_grad) <= self.epsilon:
+				break		
+		
+	
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--dataset", default="Admission_Predict.csv") # dataset that you want to extract the data from
@@ -63,6 +117,10 @@ if __name__ == '__main__':
 
 	file = args.dataset
 	x, y = import_data(file)
-	linear_regression(x, y)
 
+	# Fit a linear model using sklearn
+	# linear_regression_sklearn(x, y)
+
+	# Fit a linear model using written gradient descent algorithm
+	
 
