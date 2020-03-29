ef31365ffaae455fdc406b174a300bc7453e5590
YiHerngOng <ongyi@oregonstate.edu>
2020-03-28 17:53:26 -0700

    test Logistic regression

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 from linear_regression import Linear_Model
+from logistic_regression import Logistic_regression
+from preprocess import preprocess_Data 
 import argparse
 import os, sys
+
 
 def main(filename, prediction_feature):
     data_dir = os.getcwd() + "/data/"
     filename = data_dir + filename 
     assert os.path.isfile(filename), "Data csv not exist"
-    lm = Linear_Model(filename)   
-    lm.preprocess(prediction_feature)
-    lm.ridge_regression()
-    lm.standard_error_regression()
-
-
+    pD = preprocess_Data(filename)
+    x_train, x_test, y_train, y_test, prediction_feature = pD.preprocess(prediction_feature)
+    # lm = Linear_Model(x_train, x_test, y_train, y_test, prediction_feature)   
+    # lm.ridge_regression()
+    # lm.standard_error_regression()
+    logreg = Logistic_regression(x_train, x_test, y_train, y_test, prediction_feature)
+    logreg.train()
+    logreg.prediction()
+    
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--filename", default = "Admission_Predict.csv", type = str) # dataset that you want to extract the data from
